350f9223954e3a173a06aa3a8596afb8bdcafb20
Nitish-Sejwal <n6833965@gmail.com>
2025-10-31 19:03:40 +0530

    Fix Render port binding and dependencies

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -56,7 +56,4 @@ def predict():
     return jsonify({'label': label})
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))  # Render gives the port number in $PORT
-    app.run(host='0.0.0.0', port=port, debug=False)
-
+    app.run(debug=True)
